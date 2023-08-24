eb58c02992353de09449db1836ee61fa03a47e41
Devin Gundaniya <devin.desilo.studio@gmail.com>
2023-08-24 11:17:36 +0530

    setup

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,16 @@ app.use('/api/user', userRoutes);
 app.use('/api/chat',chatRoutes);
 app.use('/api/message',messageRoutes);
 
+app.get('/test', function(req,res){
+    var options = {
+       root: path.join(__dirname)
+    }
+   
+    var fileName = 'index.html'; 
+   
+    res.sendFile(fileName, options);
+   })
+
 // Deplotment 
 const __dirname1 = path.resolve();
 if(process.env.NODE_ENV === 'production') {
@@ -48,45 +58,52 @@ const PORT = process.env.PORT || 5000
 const server = app.listen(5000, console.log(`server Started on port ${PORT}`));
 console.log('socket code start');
 console.log(server)
-const io = require('socket.io')(server, {
-    pingTimeout: 60000
-});
+const io = require('socket.io')(server);
 
 
 console.log('socket io', io);
-io.on("connection",  (socket) => {
-    console.log("connected to socket.io");
+// io.on("connection",  (socket) => {
+//     console.log("connected to socket.io");
 
-    socket.on('setup', (userData)=> {
-        socket.join(userData._id);
-        console.log(userData._id)
-        socket.emit('Connected');
-    });
-
-
-    socket.on('join chat', (room)=> {
-        socket.join(room);
-        console.log("user joined room:" + room)
-        // socket.emit('Connected');
-    });
+//     socket.on('setup', (userData)=> {
+//         socket.join(userData._id);
+//         console.log(userData._id)
+//         socket.emit('Connected');
+//     });
 
 
-    socket.on('typing', (room)=>socket.in(room).emit("typing"))
-    socket.on('stop typing', (room)=>socket.in(room).emit("stop typing"))
+//     socket.on('join chat', (room)=> {
+//         socket.join(room);
+//         console.log("user joined room:" + room)
+//         // socket.emit('Connected');
+//     });
+
+//     socket.on('typing', (room)=>socket.in(room).emit("typing"))
+//     socket.on('stop typing', (room)=>socket.in(room).emit("stop typing"))
 
 
-    socket.on('new message', (newMessageRecieved) => {
-        var chat  = newMessageRecieved.chat;
+//     socket.on('new message', (newMessageRecieved) => {
+//         var chat  = newMessageRecieved.chat;
 
-        if(!chat.users) return console.log('chat . Users Not Defind');
+//         if(!chat.users) return console.log('chat . Users Not Defind');
         
-        chat.users.forEach(user => {
-            if(user._id == newMessageRecieved.sender._id) return;
+//         chat.users.forEach(user => {
+//             if(user._id == newMessageRecieved.sender._id) return;
 
-            socket.in(user._id).emit("message recieved", newMessageRecieved)
-        });
-    })
+//             socket.in(user._id).emit("message recieved", newMessageRecieved)
+//         });
+//     })
 
+// })
+
+
+io.on('connection', (server) => {
+    console.log('a user connect');
+
+    server.on('disconnect',() => {
+        console.log('Disconnected');
+    } )
 })
+
 console.log('a', io)
 console.log('socket code end -------');
