55b4403a9df61634a8dedd30873de23e8db0eecc
Devin Gundaniya <devin.desilo.studio@gmail.com>
2023-08-24 11:23:25 +0530

    setup

--- backend/server.js
+++ backend/server.js
@@ -62,48 +62,48 @@ const io = require('socket.io')(server);
 
 
 console.log('socket io', io);
-// io.on("connection",  (socket) => {
-//     console.log("connected to socket.io");
+io.on("connection",  (socket) => {
+    console.log("connected to socket.io");
 
-//     socket.on('setup', (userData)=> {
-//         socket.join(userData._id);
-//         console.log(userData._id)
-//         socket.emit('Connected');
-//     });
-
-
-//     socket.on('join chat', (room)=> {
-//         socket.join(room);
-//         console.log("user joined room:" + room)
-//         // socket.emit('Connected');
-//     });
-
-//     socket.on('typing', (room)=>socket.in(room).emit("typing"))
-//     socket.on('stop typing', (room)=>socket.in(room).emit("stop typing"))
+    socket.on('setup', (userData)=> {
+        socket.join(userData._id);
+        console.log(userData._id)
+        socket.emit('Connected');
+    });
 
 
-//     socket.on('new message', (newMessageRecieved) => {
-//         var chat  = newMessageRecieved.chat;
+    socket.on('join chat', (room)=> {
+        socket.join(room);
+        console.log("user joined room:" + room)
+        // socket.emit('Connected');
+    });
 
-//         if(!chat.users) return console.log('chat . Users Not Defind');
+    socket.on('typing', (room)=>socket.in(room).emit("typing"))
+    socket.on('stop typing', (room)=>socket.in(room).emit("stop typing"))
+
+
+    socket.on('new message', (newMessageRecieved) => {
+        var chat  = newMessageRecieved.chat;
+
+        if(!chat.users) return console.log('chat . Users Not Defind');
         
-//         chat.users.forEach(user => {
-//             if(user._id == newMessageRecieved.sender._id) return;
+        chat.users.forEach(user => {
+            if(user._id == newMessageRecieved.sender._id) return;
 
-//             socket.in(user._id).emit("message recieved", newMessageRecieved)
-//         });
-//     })
+            socket.in(user._id).emit("message recieved", newMessageRecieved)
+        });
+    })
 
-// })
-
-
-io.on('connection', (server) => {
-    console.log('a user connect');
-
-    server.on('disconnect',() => {
-        console.log('Disconnected');
-    } )
 })
+
+
+// io.on('connection', (server) => {
+//     console.log('a user connect');
+
+//     server.on('disconnect',() => {
+//         console.log('Disconnected');
+//     } )
+// })
 
 console.log('a', io)
 console.log('socket code end -------');
